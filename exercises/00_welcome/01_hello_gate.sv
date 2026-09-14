0abccf5e38845cc8e5ead1812a975f86adf75654
KaitoTLex <renl@kaitotlex.systems>
2026-09-14 11:42:01 -0700

    feat: clearer instructions for the first module

--- exercises/00_welcome/01_hello_gate.sv
+++ exercises/00_welcome/01_hello_gate.sv
@@ -2,14 +2,17 @@
 //
 // This first exercise isn't really about SystemVerilog yet - it's about
 // getting comfortable with the loop you'll repeat for the rest of the
-// course:
+// course as well as a sanity check to make sure that you have all the
+// tools and development ready:
 //
 //   1. read the broken thing below
 //   2. fix it
 //   3. save the file
 //   4. run:  svlings verify
 //
-// That's it. That's the whole course, one small circuit at a time.
+//   If you can't run svlings, double check; do you have `Nix` installed?
+//   Are you in the development shell either using `nix develop` or `direnv allow`
+//   If you have done none of the above visit `https://cdi-sjsu.github.io/grimoire/docs/devenv/`
 //
 // ---------------------------------------------------------------------
 //
@@ -25,10 +28,10 @@ module hello_gate (
     output logic led
 );
 
-    // TODO: the line below is a comment (it starts with "//"), so
-    // SystemVerilog skips right over it. Delete the "// " at the start
-    // of the next line so it actually takes effect.
+  // TODO: the line below is a comment (it starts with "//"), so
+  // SystemVerilog skips right over it. Delete the "// " at the start
+  // of the next line so it actually takes effect.
 
-    // assign led = button;
+  // assign led = button;
 
 endmodule
